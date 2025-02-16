c8a630634e3c989bcad3f67246917961aeb97a7c
Nakile Solutions <31482656+NakileSolutions@users.noreply.github.com>
2025-02-16 22:30:54 +0100

    feat: added auth guard (#10)

--- applications/capsule-backend/src/app/app.module.ts
+++ applications/capsule-backend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { MagazineModule } from "./magazine/magazine.module";
+import { MagazineModule } from './magazine/magazine.module';
+import { AuthJWTModule } from '@capsule/auth';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MagazineModule],
+  imports: [MagazineModule, AuthModule, AuthJWTModule],
   controllers: [AppController],
   providers: [AppService],
 })
